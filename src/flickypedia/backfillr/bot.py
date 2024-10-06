7150e6661a95096a1e11081b28d13e40de6e1d4a
DaxServer <daxserver@icloud.com>
2024-10-06 21:29:18 +0200

    Catch all exceptions in exception handler

--- src/flickypedia/backfillr/bot.py
+++ src/flickypedia/backfillr/bot.py
@@ -145,15 +145,11 @@ class CuratorBot:
                 try:
                     start = perf_counter()
                     user_url = flickr_url_parser.parse_flickr_url(flickr_id["url"])["user_url"]
-
-                    if user_url is None:
-                        pywikibot.warning(f"Unable to find user URL")
-
                     user = flickr_api.get_user(user_url=user_url)
                     pywikibot.info(f"Retrieved Flickr user in {(perf_counter() - start) * 1000:.0f} ms")
 
                     new_claims = create_sdc_claims_for_existing_flickr_photo(user=user, photo_id=flickr_id["photo_id"], photo_url=flickr_id["url"])
-                except (ResourceNotFound, UserDeleted) as e:
+                except Exception as e:
                     pywikibot.warning(f"{flickr_id['photo_id']} warning: {e}")
                     continue
 
