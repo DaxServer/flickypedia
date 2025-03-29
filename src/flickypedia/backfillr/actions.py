823993fb422164c16ae5ccdc69935f68d59c6985
DaxServer <7479937+DaxServer@users.noreply.github.com>
2025-03-29 09:55:15 +0100

    fix: apply US PD fixes to existing edited files (#15)
    * fix: apply US PD fixes to existing edited files
    
    * feat: add support for removing statements
    
    * fix: add missing wheel
    
    * fix: space in US PD templates and commit missing code
    
    * chore: log when file is skipped
    
    * fix: time units

--- src/flickypedia/backfillr/actions.py
+++ src/flickypedia/backfillr/actions.py
@@ -9,6 +9,7 @@ from flickypedia.structured_data import (
     NewClaims,
     NewStatement,
     WikidataProperties as WP,
+    WikidataEntities,
 )
 from .comparisons import (
     are_equivalent_qualifiers,
@@ -43,16 +44,22 @@ class ReplaceStatement(typing.TypedDict):
     statement: NewStatement
 
 
+class RemoveStatement(typing.TypedDict):
+    property_id: str
+    action: typing.Literal["remove_statement"]
+    statement_id: str
+
+
 class Unknown(typing.TypedDict):
     property_id: str
     action: typing.Literal["unknown"]
 
 
-Action = DoNothing | AddMissing | AddQualifiers | ReplaceStatement | Unknown
+Action = DoNothing | AddMissing | AddQualifiers | ReplaceStatement | RemoveStatement | Unknown
 
 
 def create_actions(
-    existing_claims: ExistingClaims, new_claims: NewClaims, user: FlickrUser | None
+    existing_claims: ExistingClaims, new_claims: NewClaims, user: FlickrUser | None, is_us_pd: bool = False
 ) -> list[Action]:
     actions: list[Action] = []
 
@@ -73,6 +80,26 @@ def create_actions(
                 )
             )
             continue
+
+        if property_id == WP.CopyrightStatus and is_us_pd and len(existing_statements) > 0:
+            if (len(existing_statements) == 1 and
+                    not are_equivalent_snaks(existing_statements[0]["mainsnak"], new_statement["mainsnak"]) and
+                    existing_statements[0]["mainsnak"]["datavalue"]["value"]["id"] == WikidataEntities.DedicatedToPublicDomainByCopyrightOwner):
+                actions.append(ReplaceStatement(property_id=property_id, action="replace_statement",
+                                                statement_id=existing_statements[0]["id"], statement=new_statement))
+                continue
+
+            allowed_ids = [WikidataEntities.DedicatedToPublicDomainByCopyrightOwner, WikidataEntities.PublicDomain]
+            if len(existing_statements) == 2 and all([st["mainsnak"]["datavalue"]["value"]["id"] in allowed_ids for st in existing_statements]):
+                for statement in existing_statements:
+                    match statement["mainsnak"]["datavalue"]["value"]["id"]:
+                        case WikidataEntities.DedicatedToPublicDomainByCopyrightOwner:
+                            actions.append(RemoveStatement(property_id=property_id, action="remove_statement", statement_id=statement["id"]))
+                        case WikidataEntities.PublicDomain:
+                            actions.append(ReplaceStatement(property_id=property_id, action="replace_statement", statement_id=statement["id"], statement=new_statement))
+                continue
+
+            actions.append(DoNothing(property_id=property_id, action="do_nothing"))
 
         # We update License, Copyright status if it is the same as existing and there is only one
         # or add one if there is none
--- src/flickypedia/backfillr/bot.py
+++ src/flickypedia/backfillr/bot.py
@@ -11,7 +11,7 @@ import pywikibot
 from deepdiff import DeepDiff
 from flickr_photos_api import FlickrApi, PhotoIsPrivate, ResourceNotFound
 from httpx import Client
-from pywikibot import Site
+from pywikibot import Site, Page
 from pywikibot.pagegenerators import SearchPageGenerator
 
 from flickypedia.apis import WikimediaApi
@@ -43,10 +43,12 @@ class CuratorBot:
 
         self.http_client = Client(headers={"User-Agent": self.user_agent})
         self.wikimedia_api = WikimediaApi(client=self.http_client)
-        self.pd_us_templates = []
+        self.pd_us_templates = [t['title'] for t in httpx.get('https://petscan.wmcloud.org/?psid=33444757&format=json').json()['*'][0]['a']['*']]
 
-    def update(self, mid: str, summary: str, existing_claims, new_claims, user = None) -> None:
-        actions = create_actions(existing_claims, new_claims, user)
+        self.flickr_api = FlickrApi.with_api_key(api_key=os.getenv("FLICKR_API_KEY"), user_agent=self.user_agent)
+
+    def update(self, mid: str, summary: str, existing_claims, new_claims, user = None, is_us_pd: bool = False) -> None:
+        actions = create_actions(existing_claims, new_claims, user, is_us_pd)
         pywikibot.debug(actions)
 
         claims = []
@@ -60,6 +62,11 @@ class CuratorBot:
                 statement = a["statement"]
                 statement["id"] = a["statement_id"]
                 claims.append(statement)
+            elif a["action"] == "remove_statement":
+                claims.append({
+                    "id": a["statement_id"],
+                    "remove": "",
+                })
             else:
                 raise ValueError(f"Unrecognised action: {a['action']}")
 
@@ -85,7 +92,7 @@ class CuratorBot:
         try:
             start = perf_counter()
             request.submit()
-            pywikibot.info(f"Updating {mid} took {(perf_counter() - start) * 1000:.0f}s")
+            pywikibot.info(f"Updating {mid} took {(perf_counter() - start):.1f} s")
         except Exception as e:
             pywikibot.critical(f"Failed to update: {e}")
 
@@ -100,99 +107,117 @@ class CuratorBot:
         return existing_claims
 
     def is_us_pd(self, raw_extracted_templates) -> bool:
-        templates = [t for (t, x) in raw_extracted_templates]
+        templates = [t.replace(' ', '_') for (t, x) in raw_extracted_templates]
 
         return any([t in self.pd_us_templates for t in templates])
 
-    def flickr(self) -> None:
-        self.pd_us_templates = [t['title'] for t in httpx.get('https://petscan.wmcloud.org/?psid=33444757&format=json').json()['*'][0]['a']['*']]
+    def process_page(self, page: Page, summary: str | None = None, inject_us_pd: bool = False) -> None:
+        page_id = page.pageid
+        mid = f"M{page_id}"
+        pywikibot.info(f"Processing {mid}")
 
-        flickr_api = FlickrApi.with_api_key(api_key=os.getenv("FLICKR_API_KEY"), user_agent=self.user_agent)
-        generator = SearchPageGenerator("file: insource:/Category:(Files from )?Flickr/i -haswbstatement:P170", site=self.site)
+        filename = page.title()
+        pywikibot.info(f"URL for {mid}: {page.full_url()}")
 
-        for page in generator:
-            page_id = str(page.pageid)
-            mid = f"M{page_id}"
-            pywikibot.info(f"Processing {mid}")
+        existing_claims = self.get_existing_claims(mid)
+        pywikibot.debug(existing_claims)
 
-            filename = page.title()
-            pywikibot.info(f"URL for {mid}: {page.full_url()}")
+        start = perf_counter()
+        wikitext_parsed = self.wikimedia_api.get_wikitext(fileid=page_id, filename=filename)
+        pywikibot.info(f"Retrieved parsed wikitext in {(perf_counter() - start) * 1000:.0f} ms")
+        pywikibot.debug(wikitext_parsed)
 
-            existing_claims = self.get_existing_claims(mid)
-            pywikibot.debug(existing_claims)
+        is_us_pd = self.is_us_pd(page.raw_extracted_templates)
+        pywikibot.info(f"Is US PD: {is_us_pd}")
 
+        try:
+            flickr_id = find_flickr_photo_id_from_sdc(existing_claims)
+
+            if flickr_id is None or flickr_id["url"] is None:
+                flickr_id_wikitext = find_flickr_photo_id_from_parsed_wikitext(wikitext_parsed)
+
+                if flickr_id_wikitext is not None:
+                    if flickr_id is not None and flickr_id["photo_id"] != flickr_id_wikitext["photo_id"]:
+                        pywikibot.error(f"Photo ID mismatch: SDC {flickr_id} vs Wikitext {flickr_id_wikitext}")
+                        return
+                    flickr_id = flickr_id_wikitext
+        except Exception as e:
+            pywikibot.warning(f"Warning: {e}")
+            flickr_id = None
+
+        if flickr_id is None:
+            pywikibot.error("Unable to find Flickr ID")
+            return
+
+        pywikibot.info(f"Flickr ID: {flickr_id}")
+
+        try:
             start = perf_counter()
-            wikitext_parsed = self.wikimedia_api.get_wikitext(fileid=int(page_id), filename=filename)
-            pywikibot.info(f"Retrieved parsed wikitext in {(perf_counter() - start) * 1000:.0f} ms")
-            pywikibot.debug(wikitext_parsed)
-
-            is_us_pd = self.is_us_pd(page.raw_extracted_templates)
-            pywikibot.info(f"Is US PD: {is_us_pd}")
-
-            try:
-                flickr_id = find_flickr_photo_id_from_sdc(existing_claims)
-
-                if flickr_id is None or flickr_id["url"] is None:
-                    flickr_id_wikitext = find_flickr_photo_id_from_parsed_wikitext(wikitext_parsed)
-
-                    if flickr_id_wikitext is not None:
-                        if flickr_id is not None and flickr_id["photo_id"] != flickr_id_wikitext["photo_id"]:
-                            pywikibot.error(f"Photo ID mismatch: SDC {flickr_id} vs Wikitext {flickr_id_wikitext}")
-                            continue
-                        flickr_id = flickr_id_wikitext
-            except Exception as e:
-                pywikibot.warning(f"Warning: {e}")
-                flickr_id = None
-
-            if flickr_id is None:
-                pywikibot.error("Unable to find Flickr ID")
-                continue
-
-            pywikibot.info(f"Flickr ID: {flickr_id}")
+            single_photo = self.flickr_api.get_single_photo(photo_id=flickr_id["photo_id"])
+            pywikibot.info(f"Retrieved Flickr photo in {(perf_counter() - start) * 1000:.0f} ms")
+            new_claims = create_sdc_claims_for_existing_flickr_photo(photo=single_photo, is_us_pd=is_us_pd)
+            user = single_photo["owner"]
+        except (PhotoIsPrivate, ResourceNotFound) as e:
+            pywikibot.warning(f"{flickr_id['photo_id']} warning: {e}")
 
             try:
                 start = perf_counter()
-                single_photo = flickr_api.get_single_photo(photo_id=flickr_id["photo_id"])
-                pywikibot.info(f"Retrieved Flickr photo in {(perf_counter() - start) * 1000:.0f} ms")
-                new_claims = create_sdc_claims_for_existing_flickr_photo(photo=single_photo, is_us_pd=is_us_pd)
-                user = single_photo["owner"]
-            except (PhotoIsPrivate, ResourceNotFound) as e:
-                pywikibot.warning(f"{flickr_id['photo_id']} warning: {e}")
+                user_url = flickr_url_parser.parse_flickr_url(flickr_id["url"])["user_url"]
+                user = self.flickr_api.get_user(user_url=user_url)
+                pywikibot.info(f"Retrieved Flickr user in {(perf_counter() - start) * 1000:.0f} ms")
 
-                try:
-                    start = perf_counter()
-                    user_url = flickr_url_parser.parse_flickr_url(flickr_id["url"])["user_url"]
-                    user = flickr_api.get_user(user_url=user_url)
-                    pywikibot.info(f"Retrieved Flickr user in {(perf_counter() - start) * 1000:.0f} ms")
-
-                    new_claims = create_sdc_claims_for_existing_flickr_photo(user=user, photo_id=flickr_id["photo_id"], photo_url=flickr_id["url"], is_us_pd=is_us_pd)
-                except Exception as e:
-                    pywikibot.warning(f"{flickr_id['photo_id']} warning: {e}")
-                    continue
+                new_claims = create_sdc_claims_for_existing_flickr_photo(user=user, photo_id=flickr_id["photo_id"],
+                                                                         photo_url=flickr_id["url"], is_us_pd=is_us_pd)
             except Exception as e:
                 pywikibot.warning(f"{flickr_id['photo_id']} warning: {e}")
-                time.sleep(60)
+                return
+        except Exception as e:
+            pywikibot.warning(f"{flickr_id['photo_id']} warning: {e}")
+            time.sleep(60)
+            return
+
+        pywikibot.debug(new_claims)
+        pywikibot.debug(user)
+
+        self.update(
+            mid,
+            summary or "Update [[Commons:Structured data|SDC]] based on metadata from Flickr. Task #2",
+            existing_claims,
+            new_claims,
+            user,
+            inject_us_pd and is_us_pd,
+        )
+
+    def flickr(self) -> None:
+        search = 'file: deepcat:"Files from Flickr" -haswbstatement:P170'
+        pywikibot.info(search)
+        generator = SearchPageGenerator(search, site=self.site)
+
+        for page in generator:
+            self.process_page(page)
+
+    def flickr_fix(self):
+        search = 'file: deepcat:"Files from Flickr" haswbstatement:P6216=Q88088423'
+        pywikibot.info(search)
+        generator = SearchPageGenerator(search, site=self.site)
+
+        for page in generator:
+            if 'CuratorBot' not in page.contributors():
+                pywikibot.info(f"Skipping {page.title()} as it was not edited by CuratorBot")
                 continue
 
-            pywikibot.debug(new_claims)
-            pywikibot.debug(user)
-
-            self.update(
-                mid,
-                "Update [[Commons:Structured data|SDC]] based on metadata from Flickr. Task #2",
-                existing_claims,
-                new_claims,
-                user,
-            )
+            self.process_page(page, "Fix US PD [[Commons:Structured data|SDC]] based on metadata from Flickr. Task #2, see [[User_talk:DaxServer/Archive_2#CuratorBot_adding_incorrect_copyright_statements|discussion]]", True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--module", help="Which module to run", required=True, type=str, choices=["flickr"])
+    parser.add_argument("--module", help="Which module to run", required=True, type=str, choices=["flickr", "flickrfix"])
 
     args = parser.parse_args()
 
     if args.module == "flickr":
         CuratorBot(args).flickr()
+    elif args.module == "flickrfix":
+        CuratorBot(args).flickr_fix()
     else:
         raise ValueError("Invalid module")
